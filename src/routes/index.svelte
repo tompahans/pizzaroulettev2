b7d3915f0ae1580a6cf99804f88ca2e2e82bda37
Tommy Jämsä <hassemedh@gmail.com>
2022-07-26 21:37:44 +0200

    style: add simple animation and randomize function

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,54 @@
 <script>
-import {pizza} from "../stores/pizzastore"
-console.log($pizza)
+import {pizza, fetchPizza} from "../stores/pizzastore"
+
 
 </script>
 
-<div id="container">
-  <h1 class="animate">Pizza Roulette</h1>
-  <button>Slumpa</button>
+<h1 class="text-4xl text-center my-8">Pizza Roulette</h1>
+
+<div class='pizzaContainer mx-auto'>
+<div class='container mx-auto bg-black bg-opacity-30 text-center p-4'>
+
+
+{#each $pizza as pizzor}
+
+<div class='text-6xl my-4 text-red-700 font-extrabold text-shadow-md'>{pizzor.id}. {pizzor.pizzaName}</div>
+<div class='pizzaIng text-4xl font-extrabold text-green-700 my-4 text-shadow-md'>{pizzor.pizzaIng}</div>
+{/each}
+
 
 
 </div>
+</div>
+<button class='mx-auto bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' on:click={fetchPizza}>Slumpa</button>
+
+
 
 
 <style>
-  /* if needed */
+  .pizzaContainer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: url("./image_piazza.png") no-repeat center;
+    max-width: 720px;
+    max-height: 720px;
+    aspect-ratio: 1/1;
+    background-size: 118%;
+    overflow: hidden;
+  vertical-align: middle;
+    clip-path: circle(50%);
+    animation: spin 250ms linear 20;
+  }
+
+  @keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+}
+
 </style>
---
+++ src/stores/pizzastore.js
@@ -0,0 +1,23 @@
+import { writable } from 'svelte/store';
+
+export const pizza = writable([]);
+
+function getRndInteger(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+export const fetchPizza = async () => {
+	const random = getRndInteger(1, 64);
+
+	const url = `http://localhost:3000/lillaitalien/?pizzaId=${random}`;
+	const res = await fetch(url);
+	const data = await res.json();
+	const loadedPizza = data.map((data) => {
+		return {
+			pizzaName: data.pizzaName,
+			pizzaIng: data.pizzaIng,
+			id: random
+		};
+	});
+	pizza.set(loadedPizza);
+};
